04dfb07ee56b9b5df7abf37c70a09299a0a88086
barakaganai <barakaganai01@gmail.com>
2023-08-15 15:54:15 +0300

    cv-resume-generator

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+function App() {
+  return <div className="App">hello word</div>;
+}
+
+export default App;
