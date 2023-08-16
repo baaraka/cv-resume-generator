b5480d42314c3efe25814bfdfb0e5a1c3b3d1fe0
barakaganai <barakaganai01@gmail.com>
2023-08-16 17:32:54 +0300

    create why.jsx

---
+++ client/src/components/why/Why.jsx
@@ -0,0 +1,84 @@
+import "./Why.scss";
+
+function Why() {
+  return (
+    <div className="why">
+      <div className="whyContainer">
+        <h1>Why build my cv or resume with cv-resume.com?</h1>
+        <div className="wContainer">
+          <div className="aside">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 13.5H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+              />
+            </svg>
+            <h2>We are actually free</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dignissimos, mollitia deleniti culpa inventore velit, incidunt
+              officia distinctio ducimus architecto dolorum suscipit doloremque
+              iste qui excepturi facilis quod est! Et, alias.
+            </p>
+          </div>
+          <div className="aside">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 13.5H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+              />
+            </svg>
+            <h2>We are actually free</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dignissimos, mollitia deleniti culpa inventore velit, incidunt
+              officia distinctio ducimus architecto dolorum suscipit doloremque
+              iste qui excepturi facilis quod est! Et, alias.
+            </p>
+          </div>
+          <div className="aside">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 13.5H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+              />
+            </svg>
+            <h2>We are actually free</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dignissimos, mollitia deleniti culpa inventore velit, incidunt
+              officia distinctio ducimus architecto dolorum suscipit doloremque
+              iste qui excepturi facilis quod est! Et, alias.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Why;
