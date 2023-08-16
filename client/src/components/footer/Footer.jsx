224daf6d1b9ad74744989c50a6207d73f574411e
barakaganai <barakaganai01@gmail.com>
2023-08-16 17:33:24 +0300

    footer.jsx

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footerContainer">
+        <div className="footerContainerList">
+          <div className="fContainer">
+            <h1>Job seekers</h1>
+            <div className="fContainerList">
+              <span>Build a resume</span>
+              <span>Samples</span>
+              <span>Cover Letter Samples</span>
+            </div>
+          </div>
+          <div className="fContainer">
+            <h1>Job seekers</h1>
+            <div className="fContainerList">
+              <span>Build a resume</span>
+              <span>Samples</span>
+              <span>Cover Letter Samples</span>
+            </div>
+          </div>
+          <div className="fContainer">
+            <h1>Job seekers</h1>
+            <div className="fContainerList">
+              <span>Build a resume</span>
+              <span>Samples</span>
+              <span>Cover Letter Samples</span>
+            </div>
+          </div>
+          <div className="fContainer">
+            <h1>Job seekers</h1>
+            <div className="fContainerList">
+              <span>Build a resume</span>
+              <span>Samples</span>
+              <span>Cover Letter Samples</span>
+            </div>
+          </div>
+        </div>
+        <div className="footerCo">
+          <span>Copyright © cv-resume.com 2023</span>
+          <div className="footerIcon">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-twitter"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
