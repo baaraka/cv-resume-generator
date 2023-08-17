7e485fb2616b35c153060e9f05b0e167c7987034
barakaganai <barakaganai01@gmail.com>
2023-08-17 07:53:59 +0300

    create details.jsx

---
+++ client/src/components/details/Details.jsx
@@ -0,0 +1,106 @@
+import "./Details.scss";
+
+function Details() {
+  return (
+    <div className="details">
+      <div className="detailsContainer">
+        <div className="top">
+          <h1>Build your resume with the best resume builder online</h1>
+          <p>
+            The Resume.com resume builder stands out from the rest, but not only
+            because we’re the only truly free resume builder out there. We also
+            offer:
+          </p>
+          <ul>
+            <li>
+              Access to dozens of professional and creative resume templates.
+            </li>
+            <li>Editing tools you can use directly on our platform.</li>
+            <li>Ability to download and print resumes instantly.</li>
+            <li>Downloads available in PDF and plain text formatting</li>
+            <li>Unlimited sharing over email and social media</li>
+            <li>
+              24/7/365 access to your resume through your resume.com account.
+            </li>
+          </ul>
+        </div>
+
+        <div className="bottom">
+          <h1>Frequently asked questions about our resume builder.</h1>
+          <div className="bottomList">
+            <div className="bottomListItem">
+              <h2>
+                Do I need a different resume for every different job
+                application?
+              </h2>
+              <p>
+                Every time you apply for a new job, you should make sure the
+                resume speaks directly to the job description. That means, your
+                resume may not need to be entirely different, but you’ll likely
+                want to make at least a few minor updates. If you’re applying
+                for a different type of job, you may want a completely different
+                resume, from the content all the way to the format. With all the
+                different templates to choose from, take advantage of our resume
+                builder and create a variety of resumes to fit both your
+                personality and your different job applications.
+              </p>
+            </div>
+            <div className="bottomListItem">
+              <h2>
+                Do I need a different resume for every different job
+                application?
+              </h2>
+              <p>
+                Every time you apply for a new job, you should make sure the
+                resume speaks directly to the job description. That means, your
+                resume may not need to be entirely different, but you’ll likely
+                want to make at least a few minor updates. If you’re applying
+                for a different type of job, you may want a completely different
+                resume, from the content all the way to the format. With all the
+                different templates to choose from, take advantage of our resume
+                builder and create a variety of resumes to fit both your
+                personality and your different job applications.
+              </p>
+            </div>
+            <div className="bottomListItem">
+              <h2>
+                Do I need a different resume for every different job
+                application?
+              </h2>
+              <p>
+                Every time you apply for a new job, you should make sure the
+                resume speaks directly to the job description. That means, your
+                resume may not need to be entirely different, but you’ll likely
+                want to make at least a few minor updates. If you’re applying
+                for a different type of job, you may want a completely different
+                resume, from the content all the way to the format. With all the
+                different templates to choose from, take advantage of our resume
+                builder and create a variety of resumes to fit both your
+                personality and your different job applications.
+              </p>
+            </div>
+            <div className="bottomListItem">
+              <h2>
+                Do I need a different resume for every different job
+                application?
+              </h2>
+              <p>
+                Every time you apply for a new job, you should make sure the
+                resume speaks directly to the job description. That means, your
+                resume may not need to be entirely different, but you’ll likely
+                want to make at least a few minor updates. If you’re applying
+                for a different type of job, you may want a completely different
+                resume, from the content all the way to the format. With all the
+                different templates to choose from, take advantage of our resume
+                builder and create a variety of resumes to fit both your
+                personality and your different job applications.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
