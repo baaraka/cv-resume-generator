517e1c19f54b69724c04cf7d05f75c7454249f11
barakaganai <barakaganai01@gmail.com>
2023-08-17 07:52:30 +0300

    create resume.jsx

---
+++ client/src/components/resume/Resume.jsx
@@ -0,0 +1,14 @@
+import "./Resume.scss";
+
+function Resume() {
+  return (
+    <div className="resume">
+      <div className="resumeContainer">
+        <h1>Start building your resume today, land your dream job tomorrow</h1>
+        <button>Go To My Resume</button>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
