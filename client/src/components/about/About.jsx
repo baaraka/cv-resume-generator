06905bebe48d1311e8121762bbf878b1b0d102b1
barakaganai <barakaganai01@gmail.com>
2023-08-18 23:11:33 +0300

    create about.jsx

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,95 @@
+import "./About.scss";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="aboutContainer">
+        <h1>Secure Your dream Job with Our CV or Resume Building Tool</h1>
+        <div className="leftRight">
+          <div className="left">
+            <img
+              src="https://images.pexels.com/photos/4101343/pexels-photo-4101343.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt="leftImg"
+            />
+          </div>
+          <div className="right">
+            <div className="aside">
+              <div className="topHeader">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                  />
+                </svg>
+                <div className="topDetails">
+                  <h2>User-Friendly CV or Resume Generator</h2>
+                  <p>
+                    Build your cv or resume easily with our step by step creator
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="aside">
+              <div className="topHeader">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                  />
+                </svg>
+                <div className="topDetails">
+                  <h2>User-Friendly CV or Resume Generator</h2>
+                  <p>
+                    Build your cv or resume easily with our step by step creator
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="aside">
+              <div className="topHeader">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                  />
+                </svg>
+                <div className="topDetails">
+                  <h2>User-Friendly CV or Resume Generator</h2>
+                  <p>
+                    Build your cv or resume easily with our step by step creator
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
