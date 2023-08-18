73b3cb454ab1a2e885b8b89dfe51ebeded5dcf93
barakaganai <barakaganai01@gmail.com>
2023-08-18 23:11:18 +0300

    import routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Home from "./pages/home/Home";
+import Page from "./pages/page/Page";
+import Create from "./pages/create/Create";
 
 function App() {
-  return <div className="App">hello word</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/resume" element={<Page />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
